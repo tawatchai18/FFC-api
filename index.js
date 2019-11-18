a45d7d74ca05f0b603886c381b5a74a81c9502ea
tawat chai <tawat1838@gmail.com>
2019-11-18 14:14:07 +0700

    ลบ ncd ที่ moc ลบ query 2-4

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const dbUrl = "mongodb://localhost:27017/ffc";
-const dbName = "ffc";
-
-FFC = require('./models/ffc');
-Pyramid = require('./models/pyramid');
-
+const FFC = require('./models/ffc');
+const Pyramid = require('./models/pyramid');
 const express = require('express');
 const app = express();
 var apicache = require('apicache');
@@ -30,12 +27,11 @@ app.get('/pyramid', cors(corsOptions), cache('12 hour'), (req, res) => {
 
 // idorg
 app.get('/pyramid/:orgId', cors(corsOptions), cache('12 hour'), (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     const orgId = req.params.orgId;
     console.log(orgId, 'perPersonData');
-
     const personDao = new FFC("person");
     const query = {"orgId": orgId, "death.date": {"$exists": false}};
-
     personDao.findToArray(query, (result) => {
         const prePerson = new Pyramid(result);
         res.json(prePerson.perPersonData());
@@ -64,47 +60,13 @@ app.get('/convert', cors(corsOptions), cache('12 hour'), (req, res) => {
 
 // อัตราส่วนผู้สูงอายุ
 app.get('/elderlyrat', cors(corsOptions), cache('12 hour'), (req, res) => {
-
     MongoClient.connect(dbUrl, {useNewUrlParser: true}, (err, db) => {
         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
         if (err) throw err;
         var dbo = db.db('ffc');
-
         var haveActivitiesQuery = {
             "death.date": {"$exists": false}, "healthAnalyze.result.ACTIVITIES": {"$exists": true}
         };
-
-        var noResultQuery = {
-            "death.date": {"$exists": false},
-            "healthAnalyze": {"$exists": true},
-            "healthAnalyze.result.ACTIVITIES.severity": null
-        };
-        var nullCount;
-
-        dbo.collection("person").find(noResultQuery).count(function (err, count) {
-            console.log(count, 'ไม่มี result');
-            nullCount = count;
-        });
-
-        var noHealthAnalyzeQuery = {
-            "death.date": {"$exists": false},
-            "healthAnalyze.result.ACTIVITIES": {"$exists": false}
-        };
-        var nullhealthanalyze;
-
-        dbo.collection("person").find(noHealthAnalyzeQuery).count(function (err, count) {
-            console.log(count, 'ไม่มีhealthanalyze');
-            nullhealthanalyze = count
-        });
-
-        var noDeathQuery = {"death.date": {"$exists": false}}
-        var total;
-
-        dbo.collection("person").find(noDeathQuery).count(function (err, count) {
-            console.log(count, 'ข้อมูลทั้งหมด');
-            total = count
-        });
-
         dbo.collection("person").find(haveActivitiesQuery).toArray((err, arr) => {
             if (err) throw err;
             if (!Array.isArray) {
@@ -112,7 +74,7 @@ app.get('/elderlyrat', cors(corsOptions), cache('12 hour'), (req, res) => {
                     return Object.prototype.toString.call(arg) === '[object Array]';
                 };
             }
-            res.json(Activ(arr, nullCount, nullhealthanalyze, total))
+            res.json(Activ(arr))
         });
     });
 });
@@ -124,7 +86,6 @@ app.get('/elderlyrat/:orgId', cors(corsOptions), cache('12 hour'), (req, res) =
         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
         if (err) throw err;
         var dbo = db.db('ffc');
-
         var q = {
             "death.date": {"$exists": false}, $or: [
                 {"healthAnalyze.result.ACTIVITIES.severity": "MID"},
@@ -133,39 +94,6 @@ app.get('/elderlyrat/:orgId', cors(corsOptions), cache('12 hour'), (req, res) =
             ],
             "orgId": orgId
         }
-
-        var q2 = {
-            "death.date": {"$exists": false},
-            "healthAnalyze": {"$exists": true},
-            "healthAnalyze.result.ACTIVITIES.severity": null,
-            "orgId": orgId
-        }
-        var nullCount;
-
-        dbo.collection("person").find(q2).count(function (err, count) {
-            console.log(count, 'ไม่มี result');
-            nullCount = count;
-        })
-
-        var q3 = {"death.date": {"$exists": false}, "healthAnalyze": {"$exists": false}, "orgId": orgId}
-        var nullhealthanalyze;
-
-        dbo.collection("person").find(q3).count(function (err, count) {
-            console.log(count, 'ไม่มีhealthanalyze');
-
-            nullhealthanalyze = count
-        })
-
-        var q4 = {"death.date": {"$exists": false}, "orgId": orgId}
-        var total;
-
-        dbo.collection("person").find(q4).count(function (err, count) {
-            console.log(count, 'ข้อมูลทั้งหมด');
-
-            total = count
-        })
-
-
         dbo.collection("person").find(q).toArray((err, arr) => {
             if (err) throw err;
             if (!Array.isArray) {
@@ -173,48 +101,17 @@ app.get('/elderlyrat/:orgId', cors(corsOptions), cache('12 hour'), (req, res) =
                     return Object.prototype.toString.call(arg) === '[object Array]';
                 };
             }
-            res.json(Activ(arr, nullCount, nullhealthanalyze, total))
+            res.json(Activ(arr))
         });
     });
 });
 
-app.get('/ncd', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.json(NCD)
-})
 
 app.listen(7000, () => {
     console.log('Application is running on port 7000')
 })
 
-const NCD = [
-    {
-        "name": "โรคเบาหวาน",
-        "peple": 479
-    },
-    {
-        "name": "โรคหลอดเลือดสมองและหัวใจ",
-        "peple": 300
-    },
-    {
-        "name": "โรคถุงลมโป่งพอง",
-        "peple": 400
-    },
-    {
-        "name": "โรคมะเร็ง",
-        "peple": 100
-    },
-    {
-        "name": "โรคความดันโลหิตสูง",
-        "peple": 100
-    },
-    {
-        "name": "อื่นๆ",
-        "peple": 100
-    }
-];
-
-function Activ(arr, nullCount, nullhealthanalyze, total) {
+function Activ(arr) {
     const data = [
         {
             0: "MID",
@@ -237,10 +134,8 @@ function Activ(arr, nullCount, nullhealthanalyze, total) {
     let Else = 0;
     console.log(Else, 'มากกว่า 60');
     arr.forEach((item) => {
-        // console.log(item.birthDate,'birthDate');
         if (item.healthAnalyze.result !== undefined) {
             var years = moment().diff(item.birthDate, 'years', false);
-            // console.log(years,item.id, 'years5555');
             if (years >= 60) {
                 if (item.healthAnalyze.result.ACTIVITIES.severity === 'MID') {
                     data['0'].mid += 1;
