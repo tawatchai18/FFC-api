233ad45e00f4f8c514dcaaf60144a124bb57ce36
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-12-02 16:15:39 +0700

    remove block.

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ app.get(rootPart + '/pyramid/:orgId', cors(corsOptions), cache('6 hour'), (req,
 });
 
 // ชื่อหน่วยงาน
-const ignoreOrg = ["5db0973f698922acf8b802fa", "5dbbc89c698922acf8bc5789", "5cad8abd698922aa8a93b4e9", "5d8b252b698922acf8ac1897", "5cfdc5d5698922acf89a92e8", "5d1c2679698922acf8a0070c", "5d4ebdba698922acf8a45770", "5d5e7a4b698922acf8a6b72d", "5d801307698922acf8ab37c0", "5d8b2523698922acf8ac17cb", "5d8b2b53698922acf8ac35b7", "5daea3eb698922acf8b6a799", "5c98b5ec698922768b67f336", "5d034a2c698922acf89cffdc", "5d59fc53698922acf8a69769"];
+const ignoreOrg = ["5dbbc89c698922acf8bc5789"];
 app.get(rootPart + '/convert', cors(corsOptions), cache('6 hour'), (req, res) => {
     const orgDao = new FFC("organ");
 
